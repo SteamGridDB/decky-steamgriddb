d733ccb3478bbd1b8f4a0875ef6fd075f4298412
Jozen <me@jozen.blue>
2022-11-11 23:54:58 +0800

    Simplify floatymcfloatbar

--- src/AssetTab.tsx
+++ src/AssetTab.tsx
@@ -7,23 +7,18 @@ import {
   joinClassNames
 } from 'decky-frontend-lib';
 import { useState, VFC, useRef } from 'react';
-import useScrollDirection from './hooks/useScrollDirection';
 import { useSGDB } from './SGDBProvider';
 import AssetImage from './components/AssetImage';
 
 import i18n from './utils/i18n';
 import log from './utils/log';
-import { useEffect } from 'react';
   
 const AssetTab: VFC = () => {
   const { appDetails, doSearch } = useSGDB();
   const [assetSize, setAssetSize] = useState<number>(120);
-  const [scrollerElement, setScrollerElement] = useState<HTMLElement | null>(null);
   
   const firstButtonRef = useRef<HTMLDivElement>(null);
   const mainContentRef = useRef<HTMLDivElement>(null);
-
-  const scrollDirection = useScrollDirection(scrollerElement, 300);
 
   const onAssetClick = () => {
     log('cliccc');
@@ -35,26 +30,15 @@ const AssetTab: VFC = () => {
     firstButtonRef.current?.focus();
   };
 
-  // Need to wait for mainContentRef parent to be ready, can't pass it into useScrollDirection() directly
-  useEffect(() => {
-    if (mainContentRef.current?.parentElement) {
-      setScrollerElement(mainContentRef.current?.parentElement);
-    }
-  }, [mainContentRef]);
-
   if (!appDetails) return null;
 
   return (<>
     <Focusable
-      className={joinClassNames('settings-container', `scrolling-${scrollDirection}`)}
+      className="settings-container"
       focusClassName="force-show"
       focusWithinClassName="force-show"
       onOKActionDescription={i18n('Change Filters')}
       onActivate={focusSettings}
-      onCancel={() => {
-        log('Abort');
-        (mainContentRef.current?.firstElementChild as HTMLElement)?.focus();
-      }}
     >
       <Focusable className="action-buttons">
         <Focusable>
