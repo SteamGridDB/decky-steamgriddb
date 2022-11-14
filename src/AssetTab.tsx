852c1faac76e1b40b2ff9eac41fe6a2521ceb748
Jozen <me@jozen.blue>
2022-11-15 00:08:07 +0800

    Extract Toolbar

--- src/AssetTab.tsx
+++ src/AssetTab.tsx
@@ -1,69 +1,21 @@
 import {
   Focusable,
-  SliderField,
-  DialogButton,
-  showModal,
-  ModalRoot,
-  SliderFieldProps,
-  Spinner,
-  SteamSpinner,
   joinClassNames
 } from 'decky-frontend-lib';
-import { useState, VFC, useRef, useEffect, useMemo } from 'react';
+import { useState, VFC, useRef, useEffect } from 'react';
 import { useSGDB } from './hooks/useSGDB';
 import Asset from './components/Asset';
 import i18n from './utils/i18n';
 import log from './utils/log';
-import useSettings from './hooks/useSettings';
-
-const sliderProps = {
-  grid_p: {
-    min: 100,
-    max: 200,
-    step: 5,
-  },
-  grid_l: {
-    min: 100,
-    max: 200,
-    step: 5,
-  },
-  hero: {
-    min: 1,
-    max: 6,
-    step: 1,
-    notchCount: 6,
-    notchTicksVisible: true
-  },
-  logo: {
-    min: 1,
-    max: 6,
-    step: 1,
-    notchCount: 6,
-    notchTicksVisible: true
-  },
-  icon: {
-    min: 100,
-    max: 200,
-    step: 5,
-  },
-};
-
-const defaultSliderSizes = {
-  grid_p: 120,
-  grid_l: 126,
-  hero: 4,
-  logo: 4,
-  icon: 120,
-};
+import Toolbar, { ToolbarRefType } from './components/Toolbar';
 
 const AssetTab: VFC<{assetType: SGDBAssetType}> = ({ assetType }) => {
-  const { settings, set } = useSettings();
   const { isSearchReady, appDetails, doSearch, changeAssetFromUrl } = useSGDB();
-  const [sliderValue, setSliderValue] = useState<number>(120);
   const [assets, setAssets] = useState<Array<any>>([]);
   const [downloading, setDownloading] = useState<boolean>(false);
+  const [sizingStyles, setSizingStyles] = useState<any>(undefined);
   
-  const firstButtonRef = useRef<HTMLDivElement>(null);
+  const toolbarRef = useRef<ToolbarRefType>(null);
   const mainContentRef = useRef<HTMLDivElement>(null);
 
   const onAssetClick = async (url: string) => {
@@ -78,26 +30,9 @@ const AssetTab: VFC<{assetType: SGDBAssetType}> = ({ assetType }) => {
     }
   };
 
-  const handleSliderChange: SliderFieldProps['onChange'] = (size) => {
-    setSliderValue(size);
-    set(`assetSize_${assetType}`, size);
-  };
-
-  const assetSizeStyleAttr = useMemo(() => {
-    if (['hero', 'logo'].includes(assetType)) {
-      const percent = 100 / (sliderProps[assetType].max - sliderValue + sliderProps[assetType].min);
-      return {
-        gridTemplateColumns: `repeat(auto-fill, minmax(calc(${percent}% - .5em), 1fr))`
-      };
-    }
-    return {
-      ['--asset-size' as string]: `${sliderValue}px`
-    };
-  }, [assetType, sliderValue]);
-
   const focusSettings = () => {
     log('focusSettings');
-    firstButtonRef.current?.focus();
+    toolbarRef.current?.focus();
   };
 
   useEffect(() => {
@@ -111,71 +46,17 @@ const AssetTab: VFC<{assetType: SGDBAssetType}> = ({ assetType }) => {
     }
   }, [assetType, doSearch, isSearchReady]);
 
-  // Set initial slider value fron config or default
-  useEffect(() => {
-    setSliderValue(settings[`assetSize_${assetType}`] ?? defaultSliderSizes[assetType]);
-  }, [assetType, settings]);
-
   if (!appDetails) return null;
 
   return (<div className="tabcontents-wrap">
-    <Focusable
-      className="settings-container"
-      focusClassName="force-show"
-      focusWithinClassName="force-show"
-      onOKActionDescription={i18n('Change Filters')}
-      onActivate={focusSettings}
-    >
-      <Focusable className="action-buttons">
-        <Focusable
-          style={{
-            alignItems: 'center',
-            display: 'flex'
-          }}
-        >
-          <DialogButton
-            ref={firstButtonRef}
-            noFocusRing
-            onOKActionDescription={i18n('Open Filters')}
-            onClick={(evt: Event) => {
-              evt.stopPropagation();
-              log('Open Filters');
-              showModal(
-                <ModalRoot bDisableBackgroundDismiss={false} bHideCloseIcon={false}>
-                  chungus
-                </ModalRoot>,
-                window,
-                {
-                  fnOnClose: () => {
-                    log('close filters modal');
-                  },
-                  strTitle: 'Search Filters',
-                }
-              );
-            }}
-          >
-            {i18n('Filter')}
-          </DialogButton>
-        </Focusable>
-        <SliderField
-          /* @ts-ignore: className is a valid prop */
-          className="size-slider"
-          onChange={handleSliderChange}
-          onClick={(evt: Event) => evt.stopPropagation()}
-          value={sliderValue}
-          layout="below"
-          bottomSeparator="none"
-          {...sliderProps[assetType]}
-        />
-      </Focusable>
-    </Focusable>
+    <Toolbar ref={toolbarRef} assetType={assetType} onSizeChange={(size) => setSizingStyles(size)} />
     <div className={joinClassNames('spinnyboi', assets.length > 0 ? 'loaded' : '')}>
       <img alt="Steam Spinner" src="/images/steam_spinner.png" />
     </div>
     <Focusable
       ref={mainContentRef}
       className="image-container"
-      style={assetSizeStyleAttr}
+      style={sizingStyles}
     >
       {assets.map((asset) => <Asset
         key={asset.id}
---
+++ src/components/Toolbar.tsx
@@ -0,0 +1,158 @@
+import {
+  Focusable,
+  SliderField,
+  DialogButton,
+  showModal,
+  ModalRoot,
+  SliderFieldProps
+} from 'decky-frontend-lib';
+import { useState, useImperativeHandle, ForwardedRef, useRef, forwardRef, useEffect, useMemo, Ref } from 'react';
+
+import i18n from '../utils/i18n';
+import log from '../utils/log';
+import useSettings from '../hooks/useSettings';
+
+const sliderProps = {
+  grid_p: {
+    min: 100,
+    max: 200,
+    step: 5,
+  },
+  grid_l: {
+    min: 100,
+    max: 200,
+    step: 5,
+  },
+  hero: {
+    min: 1,
+    max: 6,
+    step: 1,
+    notchCount: 6,
+    notchTicksVisible: true
+  },
+  logo: {
+    min: 1,
+    max: 6,
+    step: 1,
+    notchCount: 6,
+    notchTicksVisible: true
+  },
+  icon: {
+    min: 100,
+    max: 200,
+    step: 5,
+  },
+};
+
+const defaultSliderSizes = {
+  grid_p: 120,
+  grid_l: 126,
+  hero: 4,
+  logo: 4,
+  icon: 120,
+};
+
+export type ToolbarProps = {
+  assetType: SGDBAssetType;
+  onSizeChange: (size: any) => void;
+};
+
+export type ToolbarRefType = {
+  focus: () => void;
+  assetSizeStyleAttr: any;
+};
+
+const Toolbar = forwardRef(({ assetType, onSizeChange }: ToolbarProps, ref: Ref<ToolbarRefType>) => {
+  const { settings, set } = useSettings();
+  const [sliderValue, setSliderValue] = useState<number>(120);
+  const toolbarFocusRef = useRef<HTMLDivElement>(null);
+
+  const handleSliderChange: SliderFieldProps['onChange'] = (size) => {
+    setSliderValue(size);
+    set(`assetSize_${assetType}`, size);
+  };
+
+  const assetSizeStyleAttr = useMemo(() => {
+    if (['hero', 'logo'].includes(assetType)) {
+      const percent = 100 / (sliderProps[assetType].max - sliderValue + sliderProps[assetType].min);
+      return {
+        gridTemplateColumns: `repeat(auto-fill, minmax(calc(${percent}% - .5em), 1fr))`
+      };
+    }
+    return {
+      ['--asset-size' as string]: `${sliderValue}px`
+    };
+  }, [assetType, sliderValue]);
+
+  useImperativeHandle(ref, () => ({
+    focus: () => {
+      toolbarFocusRef.current?.focus();
+    },
+    assetSizeStyleAttr,
+  }), [assetSizeStyleAttr]);
+
+  useEffect(() => {
+    onSizeChange?.(assetSizeStyleAttr);
+  }, [assetSizeStyleAttr, onSizeChange]);
+
+  // Set initial slider value fron config or default
+  useEffect(() => {
+    setSliderValue(settings[`assetSize_${assetType}`] ?? defaultSliderSizes[assetType]);
+  }, [assetType, settings]);
+  
+  return (
+    <Focusable
+      className="settings-container"
+      focusClassName="force-show"
+      focusWithinClassName="force-show"
+      onOKActionDescription={i18n('Change Filters')}
+      onActivate={() => toolbarFocusRef.current?.focus()}
+      onClick={(evt) => evt.preventDefault()} // Don't focus if using UI with a pointer
+    >
+      <Focusable className="action-buttons">
+        <Focusable
+          style={{
+            alignItems: 'center',
+            display: 'flex'
+          }}
+        >
+          <DialogButton
+            ref={toolbarFocusRef}
+            noFocusRing
+            onOKActionDescription={i18n('Open Filters')}
+            onClick={() => {
+              log('Open Filters');
+              showModal(
+                <ModalRoot bDisableBackgroundDismiss={false} bHideCloseIcon={false}>
+                  chungus
+                </ModalRoot>,
+                window,
+                {
+                  fnOnClose: () => {
+                    log('close filters modal');
+                  },
+                  strTitle: 'Search Filters',
+                }
+              );
+            }}
+          >
+            {i18n('Filter')}
+          </DialogButton>
+        </Focusable>
+        <SliderField
+          /* @ts-ignore: className is a valid prop */
+          className="size-slider"
+          onChange={handleSliderChange}
+          value={sliderValue}
+          layout="below"
+          bottomSeparator="none"
+          {...sliderProps[assetType]}
+        />
+      </Focusable>
+    </Focusable>
+  );
+});
+
+Toolbar.displayName = 'Toolbar';
+
+export default Toolbar;
