f44e6014728f082806419c5dcb5600695654b6b0
Jozen <me@jozen.blue>
2022-11-29 11:32:11 +0800

    Add lang names to i18n

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,121 +1,151 @@
 export const LANGS = {
 //  de: {
+//    name: 'Deutsch',
 //    strings: de,
 //    credit: []
 //  },
 //  fr: {
+//    name: 'Français',
 //    strings: fr,
 //    credit: []
 //  },
 //  it: {
+//    name: 'Italiano',
 //    strings: it,
 //    credit: []
 //  },
 //  ko: {
+//    name: '한국어',
 //    strings: ko,
 //    credit: []
 //  },
 //  es: {
+//    name: 'Español-Mexicano',
 //    strings: es,
 //    credit: []
 //  },
 //  'zh-cn': {
+//    name: '简体中文',
 //    strings: zhCn,
 //    credit: []
 //  },
 //  'zh-tw': {
+//    name: '简体中文',
 //    strings: zhTw,
 //    credit: []
 //  },
 //  ru: {
+//    name: 'Русский',
 //    strings: ru,
 //    credit: []
 //  },
 //  th: {
+//    name: 'ไทย',
 //    strings: th,
 //    credit: []
 //  },
 //  ja: {
+//    name: '日本語',
 //    strings: ja,
 //    credit: []
 //  },
 //  pt: {
+//    name: 'Português',
 //    strings: pt,
 //    credit: []
 //  },
 //  pl: {
+//    name: 'Polski',
 //    strings: pl,
 //    credit: []
 //  },
 //  da: {
+//    name: 'Dansk',
 //    strings: da,
 //    credit: []
 //  },
 //  nl: {
+//    name: 'Nederlands',
 //    strings: nl,
 //    credit: []
 //  },
 //  fi: {
+//    name: 'Suomi',
 //    strings: fi,
 //    credit: []
 //  },
 //  no: {
+//    name: 'Norsk',
 //    strings: no,
 //    credit: []
 //  },
 //  sv: {
+//    name: 'Svenska',
 //    strings: sv,
 //    credit: []
 //  },
 //  hu: {
+//    name: 'Magyar',
 //    strings: hu,
 //    credit: []
 //  },
 //  cs: {
+//    name: 'Čeština',
 //    strings: cs,
 //    credit: []
 //  },
 //  ro: {
+//    name: 'Română',
 //    strings: ro,
 //    credit: []
 //  },
 //  tr: {
+//    name: 'Türkçe',
 //    strings: tr,
 //    credit: []
 //  },
 //  ar: {
+//    name: 'العربية',
 //    strings: ar,
 //    credit: []
 //  },
 //  'pt-br': {
+//    name: 'Português-Brasil',
 //    strings: ptBr,
 //    credit: []
 //  },
 //  bg: {
+//    name: 'Български',
 //    strings: bg,
 //    credit: []
 //  },
 //  el: {
+//    name: 'Ελληνικά',
 //    strings: el,
 //    credit: []
 //  },
 //  en: {
+//    name: 'English',
 //    strings: en,
 //    credit: []
 //  }
 //  uk: {
+//    name: 'Українська',
 //    strings: uk,
 //    credit: []
 //  },
 //  'es-419': {
+//    name: 'Español-Latinoamérica',
 //    strings: es419,
 //    credit: []
 //  },
 //  vn: {
+//    name: 'Tiếng Việt',
 //    strings: vn,
 //    credit: []
 //  },
 //  'sc-sc': { // should be same as zh-cn
+//    name: '简体中文',
 //    strings: scSc,
 //    credit: []
 //  },
@@ -134,6 +164,11 @@ const getCurrentLanguage = (): string => {
 export const getCredits = (lang?: string) => {
   if (lang) return LANGS[lang]?.credit;
   return LANGS[getCurrentLanguage()]?.credit;
+};
+
+export const getLanguageName = (lang?: string): string => {
+  if (lang) return LANGS[lang]?.name;
+  return LANGS[getCurrentLanguage()]?.name;
 };
 
 /**
