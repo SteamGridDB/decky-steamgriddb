438e351f69b3aec26d46b25687f737dd06106c04
Jozen <me@jozen.blue>
2022-12-02 08:58:30 +0800

    Allow skipping debounce on setting save

--- src/hooks/useSettings.tsx
+++ src/hooks/useSettings.tsx
@@ -7,7 +7,7 @@ import log from '../utils/log';
 export const SettingsContext = createContext({});
 
 type SettingsContextType = {
-  set: (key: any, value: any) => void;
+  set: (key: any, value: any, immediate?: boolean) => void;
   get: (key: any, fallback: any) => Promise<any>;
   settings: any;
 };
@@ -20,10 +20,17 @@ export const SettingsProvider: FC<{ serverApi: ServerAPI }> = ({ serverApi, chil
     await serverApi.callPluginMethod('set_setting', setting);
   }, [serverApi]);
 
-  const set = useMemo(() => debounce(async (key, value) => {
+  const saveDb = useMemo(() => debounce(async (key, value) => {
     log('set setting state', key, value);
     setSetting({ key, value });
   }, 1500), []);
+
+  const set = useMemo(() => (key, value, immediate = false) => {
+    if (immediate) {
+      return setSetting({ key, value });
+    }
+    return saveDb(key, value);
+  }, [saveDb]) as SettingsContextType['set'];
 
   const get: SettingsContextType['get'] = useMemo(() => async (key, fallback) => {
     return (await serverApi.callPluginMethod('get_setting', { key, default: fallback })).result;
