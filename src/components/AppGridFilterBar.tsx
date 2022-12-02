e6716f62c2a8c75183d176cf483f37631d70a390
Jozen <me@jozen.blue>
2022-12-02 11:03:21 +0800

    Create AppGridFilterBar.tsx

---
+++ src/components/AppGridFilterBar.tsx
@@ -0,0 +1,32 @@
+import { findModuleChild } from 'decky-frontend-lib';
+import { FC, HTMLAttributes } from 'react';
+
+const appGridFilterHeaderClass = findModuleChild((m) => {
+  if (typeof m !== 'object') return;
+  for (const prop in m) {
+    if (typeof m[prop] === 'string' && prop === 'AppGridFilterHeader') {
+      return m[prop];
+    }
+  }
+  return;
+});
+  
+const appGridFilterTextClass = findModuleChild((m) => {
+  if (typeof m !== 'object') return;
+  for (const prop in m) {
+    if (typeof m[prop] === 'string' && prop === 'AppGridFilterText') {
+      return m[prop];
+    }
+  }
+  return;
+});
+
+const AppGridFilterBar: FC<HTMLAttributes<HTMLDivElement>> = ({ children, ...rest }) => <div {...rest}>
+  <div className={appGridFilterHeaderClass}>
+    <span className={appGridFilterTextClass}>
+      {children}
+    </span>
+  </div>
+</div>;
+
+export default AppGridFilterBar;
