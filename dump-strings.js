6a2a83905142044edcdad01d99b24479964a098e
Jozen <me@jozen.blue>
2023-02-14 13:38:55 +0800

    Update strings

--- dump-strings.js
+++ dump-strings.js
@@ -11,10 +11,15 @@ if (!fs.existsSync(INPUT_PATH)) {
 
 const content = fs.readFileSync(INPUT_PATH, 'utf8');
 
-const matches = content.matchAll(/(?<![A-z\d])trans_string\('(.+?)', ?'(.+?)'(?:, ?(?:false|!!0|!1))?\)/g);
+const matches = content.matchAll(/(?<![A-z\d])trans_string\('(.+?)', ?'(.+?)'\)/g);
+const steamMatchesArr = Array.from(content.matchAll(/(?<![A-z\d])trans_string\('(.+?)', ?'(.+?)'(?:, ?(?:true|1))\)/g));
+const steamMatchesFlat = steamMatchesArr.map((x) => x[1]);
+
 const strings = {};
 for (const [, key, str] of matches) {
-    strings[key] = str.replace(/(?:\\(.))/, '$1');
+    if (!steamMatchesFlat.includes(key)) {
+        strings[key] = str.replace(/(?:\\(.))/, '$1');
+    }
 }
 fs.writeFileSync(OUTPUT_PATH, JSON.stringify(strings, null, 2));
 console.info(`Saved ${Object.keys(strings).length} strings to ${OUTPUT_PATH}`);
