e6e4848a80cf259012b7e23da0a979e4d3380ef0
Tormak <63308171+Tormak9970@users.noreply.github.com>
2023-08-09 13:36:54 -0500

    chore: home works without refresh but we arnt wrapping

--- src/patches/squareHomePatch.tsx
+++ src/patches/squareHomePatch.tsx
@@ -13,11 +13,8 @@ import { libraryAssetImageClasses, appportraitClasses, homeCarouselClasses } fro
 import { rerenderAfterPatchUpdate } from "./patchUtils";
 
 let patch: RoutePatch | undefined;
-let shouldPatch = false;
 
 export const addSquareHomePatch = (serverApi: ServerAPI, mounting: boolean = false) => {
-// export const addSquareHomePatch = (serverApi: ServerAPI) => {
-  shouldPatch = true;
   // inject css if it isn't there already
   if (!findSP().window.document.getElementById('sgdb-square-capsules-home')) {
     const styleEl = findSP().window.document.createElement('style');
@@ -31,20 +28,18 @@ export const addSquareHomePatch = (serverApi: ServerAPI, mounting: boolean = fal
     findSP().window.document.head.append(styleEl);
   }
 
-  // ! This shouldn't be needed but there's some serious jank with the homepage route.
-  patch ??= serverApi.routerHook.addPatch('/library/home', (props) => {
+  patch = serverApi.routerHook.addPatch('/library/home', (props) => {
     afterPatch(props.children, 'type', (_: Record<string, unknown>[], ret?: any) => {
-      console.log('ret:', ret);
+      // console.log('ret:', ret);
 
-      // wrapReactType(ret, 'type');
+      // wrapReactType(ret);
       afterPatch(ret.type, 'type', (_: Record<string, unknown>[], ret2?: any) => {
         // console.info('ret2', ret2);
 
         let cache3: any = null;
         const recents = findInReactTree(ret2, (x) => x?.props && ('autoFocus' in x.props) && ('showBackground' in x.props));
-        console.log("recents:", recents);
 
-        // wrapReactType(recents, 'type');
+        // wrapReactType(recents);
         afterPatch(recents.type, 'type', (_: Record<string, unknown>[], ret3?: any) => {
           // console.info('ret3', ret3);
 
@@ -55,26 +50,24 @@ export const addSquareHomePatch = (serverApi: ServerAPI, mounting: boolean = fal
 
           const p = findInReactTree(ret3, (x) => x?.props?.games && x?.props.onItemFocus);
           afterPatch(p, 'type', (_: Record<string, unknown>[], ret4?: any) => {
-            console.log("ret4:", ret4);
+            // console.log("ret4:", ret4);
 
             cache3 = ret3;
 
-            // wrapReactType(ret4, 'type');
+            // wrapReactType(ret4);
             afterPatch(ret4.type, 'type', (_: Record<string, unknown>[], ret5?: any) => {
               // console.info('ret5', ret5);
 
-              if (shouldPatch) {
-                const size = ret5.props.children.props.children.props.nItemHeight;
-                ret5.props.children.props.children.props.nItemHeight = size;
+              const size = ret5.props.children.props.children.props.nItemHeight;
+              ret5.props.children.props.children.props.nItemHeight = size;
 
-                replacePatch(ret5.props.children.props.children.props, 'fnGetColumnWidth', ([index]) => {
-                  // Leave horizontal grid as wide
-                  if (index === 0 ) {
-                    return callOriginal;
-                  }
-                  return size - parseInt(homeCarouselClasses.LabelHeight); // this is how valve does it -.-
-                });
-              }
+              replacePatch(ret5.props.children.props.children.props, 'fnGetColumnWidth', ([index]) => {
+                // Leave horizontal grid as wide
+                if (index === 0) {
+                  return callOriginal;
+                }
+                return size - parseInt(homeCarouselClasses.LabelHeight); // this is how valve does it -.-
+              });
               return ret5;
             });
             return ret4;
@@ -89,19 +82,14 @@ export const addSquareHomePatch = (serverApi: ServerAPI, mounting: boolean = fal
   });
 
   // ? Always rerender bc onMount users land here too.
-  rerenderAfterPatchUpdate();
-  // if (!mounting) rerenderAfterPatchUpdate();
+  // rerenderAfterPatchUpdate();
+  if (!mounting) rerenderAfterPatchUpdate();
 };
 
 export function removeSquareHomePatch(serverApi: ServerAPI, unmounting: boolean = false): void {
   if (patch) {
-    // ! This should work but it doesn't, thanks valve.
-    // serverApi.routerHook.removePatch('/library/home', patch);
-    // patch = undefined;
-
-    // ? Janky workaround.
-    if (unmounting) serverApi.routerHook.removePatch('/library/home', patch);
-    shouldPatch = false;
+    serverApi.routerHook.removePatch('/library/home', patch);
+    patch = undefined;
 
     if (!unmounting) rerenderAfterPatchUpdate();
   }
