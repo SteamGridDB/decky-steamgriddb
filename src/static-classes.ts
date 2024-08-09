6cc379f27b5be83811e649688afd6865496df345
Jozen <me@jozen.blue>
2024-08-10 02:59:34 +0800

    fix(squareHomePatch): stricter library art classes match

--- src/static-classes.ts
+++ src/static-classes.ts
@@ -1,6 +1,6 @@
 import { findModule } from '@decky/ui';
 
-export const libraryAssetImageClasses = findModule((mod) => typeof mod === 'object' && mod?.PortraitImage);
+export const libraryAssetImageClasses = findModule((mod) => typeof mod === 'object' && mod?.PortraitImage && mod?.Container && mod?.LandscapeImage);
 export const gamepadLibraryClasses = findModule((mod) => typeof mod === 'object' && mod?.GamepadLibrary);
 export const homeCarouselClasses = findModule((mod) => typeof mod === 'object' && mod?.Featured && mod?.LabelHeight);
 export const appportraitClasses = findModule((mod) => typeof mod === 'object' && mod?.AppPortraitBanner);
