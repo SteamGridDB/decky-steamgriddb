f00adcf686ca5d679f1e2f83091f43ce42ea14e4
Jozen <me@jozen.blue>
2024-08-22 08:04:31 +0800

    fix: glow patch not updating live

--- src/utils/styleInjector.ts
+++ src/utils/styleInjector.ts
@@ -16,6 +16,18 @@ export const removeStyle = (id: string) => {
   existingStyleEl?.remove();
 };
 
+/**
+  Updates the style if it exists, creates the style if not.
+ */
+export const updateStyle = (id: string, css: string) => {
+  const existingStyleEl = findSP().window.document.getElementById(id);
+  if (existingStyleEl) {
+    existingStyleEl.textContent = css;
+  } else {
+    addStyle(id, css);
+  }
+};
+
 export const removeStyles = (...ids: string[]) => {
   for (let i = 0; i < ids.length; i++) {
     const elId = ids[i];
