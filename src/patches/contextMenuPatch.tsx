9116074f5bd0e719cad0cba68ab9a400d48b8129
Jozen <me@jozen.blue>
2025-07-04 08:45:09 +0800

    fix(contextMenuPatch): pain
    Also fixes button not showing the very first time
    Closes #141

--- src/patches/contextMenuPatch.tsx
+++ src/patches/contextMenuPatch.tsx
@@ -31,6 +31,32 @@ const spliceArtworkItem = (children: any[], appid: number) => {
   ));
 };
 
+// Check if correct menu by looking at the code of the onSelected function
+// Should be enough to ignore the screenshots and other menus.
+const isOpeningAppContextMenu = (items: any[]) => {
+  return items.findIndex((item) => findInReactTree(item, (x) => x?.onSelected &&
+    x.onSelected.toString().includes('AddToNewCollection')
+  )) !== -1;
+};
+
+const handleItemDupes = (items: any[]) => {
+  const sgdbIdx = items.findIndex((x: any) => x?.key === 'sgdb-change-artwork');
+  if (sgdbIdx != -1) items.splice(sgdbIdx, 1);
+};
+
+const patchMenuItems = (menuItems: any[], appid: number) => {
+  let updatedAppid: number = appid;
+  // find the first menu component that has the correct appid assigned to _owner
+  const parentOverview = menuItems.find((x: any) => x?._owner?.pendingProps?.overview?.appid &&
+    x._owner.pendingProps.overview.appid !== appid
+  );
+  // if found then use that appid
+  if (parentOverview) {
+    updatedAppid = parentOverview._owner.pendingProps.overview.appid;
+  }
+  spliceArtworkItem(menuItems, updatedAppid);
+};
+
 /**
  * Patches the game context menu.
  * @param LibraryContextMenu The game context menu.
@@ -47,34 +73,41 @@ const contextMenuPatch = (LibraryContextMenu: any) => {
     const appid: number = component._owner.pendingProps.overview.appid;
 
     if (!patches.inner) {
-      patches.inner = afterPatch(component.type.prototype, 'shouldComponentUpdate', ([nextProps]: any, shouldUpdate: any) => {
-        try {
-          const sgdbIdx = nextProps.children.findIndex((x: any) => x?.key === 'sgdb-change-artwork');
-          if (sgdbIdx != -1) nextProps.children.splice(sgdbIdx, 1);
-        } catch (error) {
-          // wrong context menu (probably)
-          return component;
-        }
-
-        if (shouldUpdate === true) {
-          let updatedAppid: number = appid;
-          // find the first menu component that has the correct appid assigned to _owner
-          const parentOverview = nextProps.children.find((x: any) => x?._owner?.pendingProps?.overview?.appid &&
-            x._owner.pendingProps.overview.appid !== appid
-          );
-          // if found then use that appid
-          if (parentOverview) {
-            updatedAppid = parentOverview._owner.pendingProps.overview.appid;
+      patches.inner = afterPatch(component, 'type', (_: any, ret: any) => {
+        // initial render
+        afterPatch(ret.type.prototype, 'render', (_: any, ret2: any) => {
+          const menuItems = ret2.props.children[0]; // always the first child
+          if (!isOpeningAppContextMenu(menuItems)) return ret2;
+          try {
+            handleItemDupes(menuItems);
+          } catch (error) {
+            // wrong context menu (probably)
+            return ret2;
           }
-          spliceArtworkItem(nextProps.children, updatedAppid);
-        }
+          patchMenuItems(menuItems, appid);
+          return ret2;
+        });
 
-        return shouldUpdate;
+        // when steam decides to regresh app overview
+        afterPatch(ret.type.prototype, 'shouldComponentUpdate', ([nextProps]: any, shouldUpdate: any) => {
+          try {
+            handleItemDupes(nextProps.children);
+          } catch (error) {
+            // wrong context menu (probably)
+            return shouldUpdate;
+          }
+
+          if (shouldUpdate === true) {
+            patchMenuItems(nextProps.children, appid);
+          }
+
+          return shouldUpdate;
+        });
+        return ret;
       });
     } else {
       spliceArtworkItem(component.props.children, appid);
     }
-
     return component;
   });
   patches.unpatch = () => {
