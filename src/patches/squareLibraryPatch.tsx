8702acd768510c603388c4d46c69e54d59e7d531
Jozen <me@jozen.blue>
2025-07-04 10:30:56 +0800

    fix(squareLibraryPatch): new Steam update moved stuff around
    Closes #142

--- src/patches/squareLibraryPatch.tsx
+++ src/patches/squareLibraryPatch.tsx
@@ -48,55 +48,65 @@ export const addSquareLibraryPatch = (mounting = false) => {
 
           const { tabs, activeTab } = findInReactTree(ret3, (x) => x?.tabs && x?.activeTab);
           const tab = tabs.find((x: any) => x.id == activeTab);
-          const collection = tab.content.props.collection;
-          // console.info('collection', collection);
-          afterPatch(tab.content, 'type', (_: Record<string, unknown>[], ret4) => {
-            // console.info('ret4', ret4);
-            if (!ret4) return ret4;
+          console.info('tab', tab);
+          if (tab.content.props?.collectionid === null) { // this is null if on the base collections page
+            return ret3;
+          }
+          if (tab.content.props.children || tab.content.props.collection || tab.content.type) { // All games, non-steam, collections
+            const collection = tab.content.props?.children || tab.content;
+            // console.info('collection', collection);
+            wrapReactType(collection.type);
+            afterPatch(collection, 'type', (_: Record<string, unknown>[], ret4) => {
+              // console.info('ret4', ret4);
+              if (!ret4) return ret4;
 
-            const p1 = findInReactTree(ret4, (x) => x?.type && x.props?.appOverviews);
-            if (p1 && collection) {
-              afterPatch(p1, 'type', (_: Record<string, unknown>[], ret5) => {
-                // console.info('ret5', ret5);
-                patchGridProps(ret5);
-                return ret5;
-              });
-            } else if (findInReactTree(ret4.props.children, (x) => x?.type && x.type.toString().includes('coverSize'))) {
-              // ignore collections page
-              return ret4;
-            } else {
-              // console.info('ret4 sc', ret4);
-              if (ret4.props.children[0].props?.collectionid) {
-                // collections
-                afterPatch(ret4.props.children[0], 'type', (_: Record<string, unknown>[], ret5) => {
-                  // console.info('ret5 c', ret5);
-                  afterPatch(findInReactTree(ret5, (x) => x?.type && x.props?.collection?.id), 'type', (_: Record<string, unknown>[], ret6) => {
-                    // console.info('ret6 c', ret6);
-                    afterPatch(findInReactTree(ret6, (x) => x?.type && x.props?.appOverviews), 'type', (_: Record<string, unknown>[], ret7) => {
-                      // console.info('ret7 c', ret7);
-                      patchGridProps(ret7);
-                      return ret7;
-                    });
-                    return ret6;
-                  });
+              const p1 = findInReactTree(ret4, (x) => x?.type && x.props?.appOverviews);
+              console.log('p1', p1);
+              if (p1 && collection) {
+                afterPatch(p1, 'type', (_: Record<string, unknown>[], ret5) => {
+                  // console.info('ret5', ret5);
+                  patchGridProps(ret5);
                   return ret5;
                 });
+              } else if (findInReactTree(ret4.props.children, (x) => x?.type && x.type.toString().includes('coverSize'))) {
+                // ignore collections page
+                // console.info('collections page');
+                return ret4;
               } else {
-                // non-steam shortcuts
-                const p2 = findInReactTree(ret4, (x) => x?.type && x.props?.collection?.id === 'deck-desktop-apps');
-                afterPatch(p2, 'type', (_: Record<string, unknown>[], ret5) => {
-                  // console.info('ret5 s', ret5);
-                  afterPatch(findInReactTree(ret5, (x) => x?.type && x.props?.appOverviews), 'type', (_: Record<string, unknown>[], ret6) => {
-                    // console.info('ret6 s', ret6);
-                    patchGridProps(ret6);
-                    return ret6;
+                // console.info('ret4 sc', ret4);
+                console.info('ret4', ret4);
+                if (ret4.props.children[0].props?.collectionid) {
+                  // collections
+                  afterPatch(ret4.props.children[0], 'type', (_: Record<string, unknown>[], ret5) => {
+                    // console.info('ret5 c', ret5);
+                    afterPatch(findInReactTree(ret5, (x) => x?.type && x.props?.collection?.id), 'type', (_: Record<string, unknown>[], ret6) => {
+                      // console.info('ret6 c', ret6);
+                      afterPatch(findInReactTree(ret6, (x) => x?.type && x.props?.appOverviews), 'type', (_: Record<string, unknown>[], ret7) => {
+                        // console.info('ret7 c', ret7);
+                        patchGridProps(ret7);
+                        return ret7;
+                      });
+                      return ret6;
+                    });
+                    return ret5;
                   });
-                  return ret5;
-                });
+                } else {
+                  // non-steam shortcuts
+                  const p2 = findInReactTree(ret4, (x) => x?.type && x.props?.collection?.id === 'deck-desktop-apps');
+                  afterPatch(p2, 'type', (_: Record<string, unknown>[], ret5) => {
+                    // console.info('ret5 s', ret5);
+                    afterPatch(findInReactTree(ret5, (x) => x?.type && x.props?.appOverviews), 'type', (_: Record<string, unknown>[], ret6) => {
+                      // console.info('ret6 s', ret6);
+                      patchGridProps(ret6);
+                      return ret6;
+                    });
+                    return ret5;
+                  });
+                }
               }
-            }
-            return ret4;
-          });
+              return ret4;
+            });
+          }
           return ret3;
         });
         return ret2;
